a626a2dd856f4cd01a09ec11093aadd98f7900c8
DudekulaFatheema <131261777+DudekulaFatheema@users.noreply.github.com>
2023-06-22 13:43:45 +0530

    Update VoterServiceImplTest.java

--- src/test/java/com/voter/service/VoterServiceImplTest.java
+++ src/test/java/com/voter/service/VoterServiceImplTest.java
@@ -37,24 +37,41 @@ public class VoterServiceImplTest {
 	
 	
 	
-	@Test
+	// @Test
 
-      void createVoter() throws NullValueFoundException{
+ //      void createVoter() throws NullValueFoundException{
 		
 		 
-		Voter sampleInput = new Voter(8,"haseena","male", 15, 90876435, "haseena","haseen4");
-		Voter expectedOutput = new Voter(8,"haseena","male", 18, 90879365, "haseen","has98");
+	// 	Voter sampleInput = new Voter(8,"haseena","male", 15, 90876435, "haseena","haseen4");
+	// 	Voter expectedOutput = new Voter(8,"haseena","male", 18, 90879365, "haseen","has98");
 		
-		VoterService voterService = mock(VoterService.class);
-        when(voterService.createVoter(sampleInput)).thenReturn(expectedOutput);
+	// 	VoterService voterService = mock(VoterService.class);
+ //        when(voterService.createVoter(sampleInput)).thenReturn(expectedOutput);
 
-        // Act
-        Voter actualOutput = voterService.createVoter(sampleInput);
+ //        // Act
+ //        Voter actualOutput = voterService.createVoter(sampleInput);
 
-        // Assert
-        assertEquals(expectedOutput, actualOutput);
-	}
-	
+ //        // Assert
+ //        assertEquals(expectedOutput, actualOutput);
+	// }
+	 @Test
+void createVoter() throws NullValueFoundException{
+ Voter voter = new Voter();
+   voter.setAge(18);
+          voter.setName("haseena");
+       voter.setGender("male");
+	voter.setPhoneNumber(56743865);
+         voter.setUserName("mario");
+         voter.setPassWord("js123");
+
+ Voter createVoter = voterService.createVoter(voter);
+
+   assertNotNull(createVoter);
+
+  }
+}
+
+
 	@Test
 	void registerVoter() throws NullUserFound, NullUserNameFoundException {
 		
