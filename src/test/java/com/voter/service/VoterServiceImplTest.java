fe685014f73aa5909de4d8d244dd990209f98bea
DudekulaFatheema <131261777+DudekulaFatheema@users.noreply.github.com>
2023-06-22 11:50:29 +0530

    Update VoterServiceImplTest.java

--- src/test/java/com/voter/service/VoterServiceImplTest.java
+++ src/test/java/com/voter/service/VoterServiceImplTest.java
@@ -29,137 +29,137 @@ import com.voter.repository.VoterRepository;
 @SpringBootTest
 public class VoterServiceImplTest {
 	
-	@Autowired
-	VoterRepository voterRepo;
+// 	@Autowired
+// 	VoterRepository voterRepo;
 	
-	@Autowired
-	VoterService voterService;
+// 	@Autowired
+// 	VoterService voterService;
 	
-	private final Logger mylogs = LoggerFactory.getLogger(this.getClass());
+// 	private final Logger mylogs = LoggerFactory.getLogger(this.getClass());
 	
-	@Test
+// 	@Test
 
-      void createVoter() throws NullValueFoundException{
+//       void createVoter() throws NullValueFoundException{
 		
 		 
-		Voter sampleInput = new Voter(8,"haseena","male", 15, 90876435, "haseena","haseen4");
-		Voter expectedOutput = new Voter(8,"haseena","male", 18, 90879365, "haseen","has98");
+// 		Voter sampleInput = new Voter(8,"haseena","male", 15, 90876435, "haseena","haseen4");
+// 		Voter expectedOutput = new Voter(8,"haseena","male", 18, 90879365, "haseen","has98");
 		
-		VoterService voterService = mock(VoterService.class);
-        when(voterService.createVoter(sampleInput)).thenReturn(expectedOutput);
+// 		VoterService voterService = mock(VoterService.class);
+//         when(voterService.createVoter(sampleInput)).thenReturn(expectedOutput);
 
-        // Act
-        Voter actualOutput = voterService.createVoter(sampleInput);
+//         // Act
+//         Voter actualOutput = voterService.createVoter(sampleInput);
 
-        // Assert
-        assertEquals(expectedOutput, actualOutput);
-	}
+//         // Assert
+//         assertEquals(expectedOutput, actualOutput);
+// 	}
 	
-	@Test
-	void registerVoter() throws NullUserFound, NullUserNameFoundException {
+// 	@Test
+// 	void registerVoter() throws NullUserFound, NullUserNameFoundException {
 		
-         Voter voter = new Voter();
-          voter.setAge(18);
-          voter.setName("haseena");
-		 voter.setGender("male");
-		 voter.setPhoneNumber(56743865);
-         voter.setUserName("mario");
-         voter.setPassWord("js123");
-       Voter registeredVoter = voterService.registerVoter(voter);
+//          Voter voter = new Voter();
+//           voter.setAge(18);
+//           voter.setName("haseena");
+// 		 voter.setGender("male");
+// 		 voter.setPhoneNumber(56743865);
+//          voter.setUserName("mario");
+//          voter.setPassWord("js123");
+//        Voter registeredVoter = voterService.registerVoter(voter);
 
-	 assertNotNull(registeredVoter);
-	 assertEquals(18, registeredVoter.getAge());
-      assertEquals("haseena", registeredVoter.getName());
-     assertEquals("male", registeredVoter.getGender());
-     assertEquals(56743865, registeredVoter.getPhoneNumber());
- assertEquals("mario", registeredVoter.getUserName());
-  assertEquals("js123", registeredVoter.getPassWord());
+// 	 assertNotNull(registeredVoter);
+// 	 assertEquals(18, registeredVoter.getAge());
+//       assertEquals("haseena", registeredVoter.getName());
+//      assertEquals("male", registeredVoter.getGender());
+//      assertEquals(56743865, registeredVoter.getPhoneNumber());
+//  assertEquals("mario", registeredVoter.getUserName());
+//   assertEquals("js123", registeredVoter.getPassWord());
 	
 	
 
-}
+// }
 	
-	@Test
-	void UpdateVoter() throws Exception {
-// Create a team member with valid ID and updated name
+// 	@Test
+// 	void UpdateVoter() throws Exception {
+// // Create a team member with valid ID and updated name
 
-		Voter sampleInput = new Voter(8,"haseena","male", 15, 90876435, "haseena","haseen4");
-		Voter expectedOutput = new Voter(8,"haseena","male", 18, 90879365, "haseen","has98");
-
-		 
-
-		VoterService voterService = Mockito.mock(VoterService.class);
-
-		BDDMockito.when(voterService.updateVoter(sampleInput)).thenReturn(expectedOutput);
+// 		Voter sampleInput = new Voter(8,"haseena","male", 15, 90876435, "haseena","haseen4");
+// 		Voter expectedOutput = new Voter(8,"haseena","male", 18, 90879365, "haseen","has98");
 
 		 
 
-		Voter voter = new Voter();
+// 		VoterService voterService = Mockito.mock(VoterService.class);
+
+// 		BDDMockito.when(voterService.updateVoter(sampleInput)).thenReturn(expectedOutput);
 
 		 
 
-		Voter actualOutput = voterService.updateVoter(sampleInput);
+// 		Voter voter = new Voter();
 
 		 
 
-		assertEquals(expectedOutput, actualOutput);
+// 		Voter actualOutput = voterService.updateVoter(sampleInput);
 
-	}
+		 
+
+// 		assertEquals(expectedOutput, actualOutput);
+
+// 	}
 	
-	@Test
+// 	@Test
 	
-	void testGetAllVoters() throws Exception{
+// 	void testGetAllVoters() throws Exception{
 		
-		 Voter voter1 = new Voter(8,"haseena","male", 15, 90876435, "haseena","haseen4");
-		Voter voter2 = new Voter(8,"haseena","male", 18, 90879365, "haseen","has98");
+// 		 Voter voter1 = new Voter(8,"haseena","male", 15, 90876435, "haseena","haseen4");
+// 		Voter voter2 = new Voter(8,"haseena","male", 18, 90879365, "haseen","has98");
 		
-	     List<Voter> sampleOutput = new ArrayList<>();
-		sampleOutput.add(voter1);
-		sampleOutput.add(voter2);
+// 	     List<Voter> sampleOutput = new ArrayList<>();
+// 		sampleOutput.add(voter1);
+// 		sampleOutput.add(voter2);
 		
-		 VoterService voterService = mock(VoterService.class);
-	        when(voterService.getAllVoters()).thenReturn(sampleOutput);
+// 		 VoterService voterService = mock(VoterService.class);
+// 	        when(voterService.getAllVoters()).thenReturn(sampleOutput);
 
-	        // Act
-	        List<Voter> actualOutput = voterService.getAllVoters();
+// 	        // Act
+// 	        List<Voter> actualOutput = voterService.getAllVoters();
 
-	        // Assert
-	        assertNotNull(actualOutput);
-	        assertIterableEquals(sampleOutput, actualOutput);
-	    }
+// 	        // Assert
+// 	        assertNotNull(actualOutput);
+// 	        assertIterableEquals(sampleOutput, actualOutput);
+// 	    }
 	
 	
-	//@Test
-	//void testDeleteByVoterId_NonExistentId() throws Exception {
-	    // Arrange
-	    //int voterId = 999;
+// 	//@Test
+// 	//void testDeleteByVoterId_NonExistentId() throws Exception {
+// 	    // Arrange
+// 	    //int voterId = 999;
 
-	    // Act and Assert
-	   // assertThrows(Exception.class, () -> voterService.deleteByVoterId(voterId));
-	    // Additional assertions can be performed to verify the exception message or behavior
-	//}
+// 	    // Act and Assert
+// 	   // assertThrows(Exception.class, () -> voterService.deleteByVoterId(voterId));
+// 	    // Additional assertions can be performed to verify the exception message or behavior
+// 	//}
 	
 	
 
 	
-	@Test
-	void testLoginVoter_EmptyPassword() {
-	    // Arrange
-	    String userName = "haseena";
-	    String password = "";
+// 	@Test
+// 	void testLoginVoter_EmptyPassword() {
+// 	    // Arrange
+// 	    String userName = "haseena";
+// 	    String password = "";
 
-	    // Act and Assert
-	    assertThrows(NullUserFound.class, () -> voterService.loginVoter(userName, password));
-	}
-	@Test
-	void testLoginVoter_EmptyUsername() {
-	    // Arrange
-	    String userName = "";
-	    String password = "haseen4";
+// 	    // Act and Assert
+// 	    assertThrows(NullUserFound.class, () -> voterService.loginVoter(userName, password));
+// 	}
+// 	@Test
+// 	void testLoginVoter_EmptyUsername() {
+// 	    // Arrange
+// 	    String userName = "";
+// 	    String password = "haseen4";
 
-	    // Act and Assert
-	    assertThrows(NullUserFound.class, () -> voterService.loginVoter(userName, password));
-	}
+// 	    // Act and Assert
+// 	    assertThrows(NullUserFound.class, () -> voterService.loginVoter(userName, password));
+// 	}
 	
 	}
 
